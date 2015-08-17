e95d4ede1c718e3ed44fcc039524c8554b4594c9
Rafi Khan <zetoslab@gmail.com>
2015-08-17 14:27:39 -0600

    Created main file

---
+++ IRSmartLib.cpp
@@ -0,0 +1,17 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  IRSmartLib.cpp
+ *
+ *    Description:  Arduino IR remote library
+ *
+ *        Version:  1.0
+ *        Created:  08/17/15 14:23:18
+ *       Revision:  none
+ *       Compiler:  Arduino IDE
+ *
+ *         Author:  z3t0, pcoughlin
+ *   Organization:  OpenIR
+ *
+ * =====================================================================================
+ */
